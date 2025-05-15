0555fd6b16677c693a4ad703ff674cdbfb0b5853
Sadiyya Fayaz <sadiyyafayaz2@gmail.com>
2025-05-15 22:13:24 +0530

    Update DQL.sql

--- SQL/DQL.sql
+++ SQL/DQL.sql
@@ -1,3 +1,18 @@
+--  It uses:
+
+-- Sorting (via ORDER BY)
+
+-- Pagination/limiting (via LIMIT)
+
+-- Ranking logic (by ordering values and choosing top)
+
 SELECT * FROM Cohort;
 
 SELECT * FROM Cohort WHERE percentage>80;
+
+-- It’s a Data Retrieval or Data Query Language (DQL) statement.
+-- Fetch the single row with the highest percentage in the Cohort table.
+SELECT *
+FROM Cohort
+ORDER BY percentage DESC
+LIMIT 2;
