7c612577af92ee00e53d3aa7deb8099deeaae483
Sadiyya Fayaz <sadiyyafayaz2@gmail.com>
2025-05-15 22:06:05 +0530

    Create DML.sql

---
+++ SQL/DML.sql
@@ -0,0 +1,3 @@
+INSERT INTO Cohort VALUES (0001, 'Clark', 'Cloud','Bangalore',70);
+INSERT INTO Cohort VALUES (0002, 'Dave', 'Dev','Mumbai',30);
+INSERT INTO Cohort VALUES (0003, 'Ava', 'front-end','Hyderabad',90);
